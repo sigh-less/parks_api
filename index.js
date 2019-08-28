4d0dda95127853639b527931d76cbb383af7de77
Silas Groskreutz <silasgroskreutz@gmail.com>
2019-08-28 10:13:01 -0500

    This version of app will search for term and return fullName, description and URL

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 // put your own value below!
 const apiKey = '5lOs1MG1I4DpqE2ST6nDUJqUHJgpd2fCKlTSDRrS'; 
-const searchURL = `https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${apiKey}`;
+const searchURL = `https://developer.nps.gov/api/v1/parks`;
+// Keeping here for reference    ?limit=${maxResults}&q=${searchTerm}&api_key=${apiKey}`;
 
 
 function formatQueryParams(params) {
@@ -16,28 +17,26 @@ function displayResults(responseJson) {
   console.log(responseJson);
   $('#results-list').empty();
   // iterate through the items array
-  for (let i = 0; i < responseJson.items.length; i++){
+  for (let i = 0; i < responseJson.data.length; i++){
     // for each video object in the items 
     //array, add a list item to the results 
     //list with the video title, description,
     //and thumbnail
     $('#results-list').append(
-      `<li><h3>${responseJson.items[i].snippet.title}</h3>
-      <p>${responseJson.items[i].snippet.description}</p>
-      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
+      `<li><h3>${responseJson.data[i].fullName}</h3>
+      <p>${responseJson.data[i].description}</p>
+      <p><a href=${responseJson.data[i].url} target="_blank">${responseJson.data[i].url}</a></p>
       </li>`
     )};
   //display the results section  
   $('#results').removeClass('hidden');
 };
 
-function getYouTubeVideos(query, maxResults=10) {
+function getParks(query, maxResults=10) {
   const params = {
-    key: apiKey,
+    api_key: apiKey,
     q: query,
-    part: 'snippet',
-    maxResults,
-    type: 'video'
+    limit: maxResults
   };
   const queryString = formatQueryParams(params)
   const url = searchURL + '?' + queryString;
@@ -62,7 +61,7 @@ function watchForm() {
     event.preventDefault();
     const searchTerm = $('#js-search-term').val();
     const maxResults = $('#js-max-results').val();
-    getYouTubeVideos(searchTerm, maxResults);
+    getParks(searchTerm, maxResults);
   });
 }
 
